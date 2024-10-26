d99ec7951056228b9a984c082b616509423dbe2f
Catalina Buzan <catalina.buzan@trackgps.ro>
2024-10-26 17:32:26 +0300

    delete from both user and device side

--- DeviceAPI/Controllers/DevicesController.cs
+++ DeviceAPI/Controllers/DevicesController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DeviceAPI.Models;
 using DeviceAPI.Context;
+using DeviceAPI.Migrations;
+using UserAPI.Data;
 
 namespace DeviceAPI.Controllers
 {
@@ -15,10 +17,12 @@ namespace DeviceAPI.Controllers
     public class DevicesController : ControllerBase
     {
         private readonly DeviceContext _context;
+        private readonly UserContext _userContext;
 
-        public DevicesController(DeviceContext context)
+        public DevicesController(DeviceContext context, UserContext userContext)
         {
             _context = context;
+            _userContext = userContext;
         }
 
         // GET: api/Devices
@@ -135,10 +139,29 @@ namespace DeviceAPI.Controllers
             _context.Device.Remove(Device);
             await _context.SaveChangesAsync();
 
+            _userContext.Remove(_userContext.Devices.Find(Device.Id));
+            _userContext.SaveChanges();
+
             return NoContent();
         }
 
+        // DELETE CASCADE: api/Devices/DeleteCascade/5
+        [HttpDelete("/DeleteCascade/({id})")]
+        public async Task<IActionResult> DeleteDeviceCascade(Guid id)
+        {
+            var Device = await _context.Device.FindAsync(id);
+            if (Device == null)
+            {
+                return NotFound();
+            }
 
+            _context.Database.ExecuteSqlRaw("CALL delete_device({0})", id);
+            await _context.SaveChangesAsync();
+
+         
+
+            return NoContent();
+        }
 
         private bool DeviceExists(Guid id)
         {
---
+++ DeviceAPI/Migrations/20241023082808_DeleteDeviceCascade.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeviceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteDeviceCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var storedProcedure = @"    
+                CREATE OR REPLACE FUNCTION delete_device(deviceId UUID)
+                RETURNS void AS $$
+                DECLARE
+                    conn_string TEXT;
+                BEGIN
+                    DELETE FROM DMS.public.Devices WHERE Id = device_id;
+
+                    conn_string := 'dbname=UMS user=postgres password=rox123 host=localhost port=5432';
+
+                    PERFORM dblink_exec(
+                        conn_string, 
+                        'DELETE FROM UMS.public.Devices WHERE Id = ''' || deviceId || ''''
+                    );
+                END;
+                $$ LANGUAGE plpgsql;
+            ";
+            migrationBuilder.Sql(storedProcedure);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
